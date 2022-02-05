e51a92b698289dcb4dc2574b7fa1404c39c949e8
juanitatime <94155823+juanitatime@users.noreply.github.com>
2022-02-05 13:06:30 +0800

    changed code placements

---
+++ scriptname.js
@@ -0,0 +1,9 @@
+font-size: 15px;
+    line-height: 1.6;
+    background-color: #EF9A9A; 
+    display: block;
+    border-left: 5px solid #77C9D4;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    padding: 15px 10px 15px 20px;
+}
